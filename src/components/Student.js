b9df30d8cbed427734f4615a6739cea1e4857a53
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-12-19 15:16:13 -0800

    created component

---
+++ src/components/Student.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Student = (props) => {
+  // Component functions always return JSX
+  return (
+    <div>
+      <h3>Ada Lovelace</h3>
+      <ul>
+        <li>Class: C13</li>
+        <li>Birthday: December 10th, 1815</li>
+        <li>Email: ada@adadev.org</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Student;
