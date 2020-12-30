3e45fa9a19333dd7a8c786120c84ccfc7c08cdab
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-12-30 13:58:48 -0800

    working on event handlers/listeners --> with fullName and setting State

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+
 
 const Student = (props) => {
   // Component functions always return JSX
+  const [present, setPresent]= useState(props.present); //  hooks -> present = variable setPresent = function to update that state
+  const [fullName, setFullName] = useState(props.fullName);
+
+  const onButtonClick = () => setPresent(!present);  //EVENT HANDLER method this is the event handler  when event is triggered
+  // taking current value of present, negating it and update value using setPresent 
+  // toggle the value will flip between true and false 
+  // any kind of code of how we choose to update of this specific component 
+
+    const onFullNameInputChange = (event) => { //EVENT HANDLER 
+        setFullName(event.target.value);
+    };
+
     return (
         <div>
-        <h3>{props.fullName}</h3>
+        <h3>{fullName}</h3>
+        <input value= {fullName} onChange={onFullNameInputChange}/>
         <ul>
             <li>Class: C13</li>
             <li>Birthday: {props.birthday}</li>
             <li>Email: {props.email}</li>
-        </ul>
+        </ul> 
+        <button onClick={onButtonClick}> 
+            Mark {present? 'Absent': 'Present'}
+        </button>
         </div>
     );
 };
-
+ // LINE 23:  event Listener (ONCLICK) when element is clicked then run the JS function on right  in curly because we use function to pass --> only reference not calling it 
 Student.propTypes = {
     fullName: PropTypes.string.isRequired,
     email: PropTypes.string,
