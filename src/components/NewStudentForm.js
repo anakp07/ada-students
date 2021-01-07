a18bdb727f5df73f9c2182045d9872d7cf90ed8e
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2021-01-07 11:23:29 -0800

    finished adding the controlled form for NewStudentForm

--- src/components/NewStudentForm.js
+++ src/components/NewStudentForm.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './NewStudentForm.css';
 
 const NewStudentForm = (props) => {
 
-    return(
-        <form className="new-student-form">
-            <div>
-                <label htmlFor="fullName">Name:</label>
-                <input name="fullName" />
-            </div>
-            <div>
-                <label htmlFor="email">Email:</label>
-                <input name="email" />
-            </div>
-            <input
-                type="submit"
-                value="Add Student"
-            />
-        </form>
-    );
-}
+    const [student, setStudent] = useState({
+        fullName: '',
+        email: '',
+    });
+
+    const onNameChange = (event) => {
+        const newStudent = {
+            ...student,  // <-- gets all fiels of students and puts them in new object
+            fullName: event.target.value
+        }
+        setStudent(newStudent);
+    };
+    
+    const onEmailChange = (event) => {
+        const newStudent = {
+            ...student,
+            email: event.target.value,
+        }
+        setStudent(newStudent);
+        }
+
+        return(
+            <form className="new-student-form">
+                <div>
+                    <label htmlFor="fullName">Name:</label>
+                    <input
+                    name="fullName"
+                    value={student.fullName}
+                    onChange={onNameChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                    name="email" 
+                    value={student.email}
+                    onChange={onEmailChange}
+                    />
+                </div>
+                <input
+                    type="submit"
+                    value="Add Student"
+                />
+            </form>
+        );
+    };
 
 
 export default NewStudentForm; 
